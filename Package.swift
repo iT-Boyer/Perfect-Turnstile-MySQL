65a7211bd160d56e30cc0a285262e8235601c8b6
布衣男儿 <iTBoyer@qq.com>
2020-06-02 10:32:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
 		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", from: "3.0.0"),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "3.0.0"),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "0.0.0"),
 		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", from: "3.0.0"),
 	]
 )
