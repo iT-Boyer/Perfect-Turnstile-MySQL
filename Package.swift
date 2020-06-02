cede3dd8f5fae0579bb0aa63a82803a19c78031d
布衣男儿 <iTBoyer@qq.com>
2020-06-02 10:28:34 +0800

    修改版本语法

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
 	name: "PerfectTurnstileMySQL",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
-		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", from: "3.0.0"),
+		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", from: "3.0.0"),
 	]
 )
